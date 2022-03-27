6e9989f623347063b54d84f750d7f811f0d2782f
l3luescreen <hacky404@hotmail.com>
2022-03-27 19:14:09 +0700

    Add account schema

---
+++ backend/src/schemas/accountSchema.ts
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const accountSchema = new Schema({
+  googleId: String,
+  businessId: String,
+  firstName: String,
+  lastName: String,
+  contact: {
+    email: String,
+    tel: String,
+  },
+  image: String,
+  setting: {
+    displayTel: Boolean,
+    weekendReceive: Boolean,
+    activeTime: {
+      startAt: String,
+      endAt: String,
+    },
+  },
+})
+
+export = accountSchema
