3e74d8b03b4afa39c5991dc868037ae2f7a1e49a
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-03-27 16:29:47 +0700

    Create a sign in page

---
+++ frontend/src/pages/signin/index.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react'
+
+type Props = {}
+
+const SignIn: FC<Props> = () => {
+    return (
+        <div className="min-h-screen bg-[color:var(--blue)] text-white">
+            <div className="container min-h-screen mx-auto px-8 flex flex-col">
+                <div className="my-auto flex flex-col justify-center items-center">
+                    <div className="text-8xl font-thin mb-8">
+                        <span className="text-[color:var(--lighter-blue)]">Sch</span>edu
+                    </div>
+                    <button className="rounded-full px-8 py-2 text-sm font-light border hover:border-blue-200 hover:text-blue-200 duration-100">
+                        sign-in with ITKMITL account
+                    </button>
+                </div>
+                <div className="py-8 text-center text-sm font-light opacity-75">
+                    <div>made by skeptical saurus team</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn
