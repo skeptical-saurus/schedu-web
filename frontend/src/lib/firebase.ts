234d8347b8c83fd4889949b57584bb8223627353
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-03-29 18:54:26 +0700

    get firebase config from file instead of env

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -1,15 +1,7 @@
 import { initializeApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_APIKEY,
-  authDomain: process.env.NEXT_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.NEXT_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.NEXT_FIREBASE_APPID,
-  measurementId: process.env.NEXT_FIREBASE_MEASUREMENTID,
-}
+import firebaseConfig from '../../secret/firebaseConfig'
 
 if (!getApps().length) {
   initializeApp(firebaseConfig)
