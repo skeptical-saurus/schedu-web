e85f5421a96408706faed8716d51a43c63ac6ece
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-04-14 15:45:13 +0700

    add button from sign in page if user already signed in

--- frontend/src/lib/cookie.ts
+++ frontend/src/lib/cookie.ts
@@ -1,12 +1,6 @@
-const getCookieValue = (cookies:String, key:String) => {
+export function getCookieValue(cookies:String, key:String) {
     cookies = `; ${cookies}`
     let parts = cookies.split(`; ${key}=`)
     if (parts.length === 2) return parts.pop()?.split(';').shift()
     return null
 }
-
-const cookieFunction = {
-    getCookieValue
-}
-
-export default cookieFunction
--- frontend/src/pages/signin/index.tsx
+++ frontend/src/pages/signin/index.tsx
@@ -1,36 +1,70 @@
-import { useEffect } from 'react'
+import { getCookieValue } from 'lib/cookie'
+import { useEffect, useState } from 'react'
 import { useAuth } from '../../context/AuthContext'
 
 type Props = {}
 
 const SignIn: React.FC<Props> = () => {
 
-    const {signIn} = useAuth()
+  const [isSigned, updateSignState] = useState(false)
 
-    useEffect(() => {
-      
-    }, [])
+  const {signIn} = useAuth()
 
-    return (
-      <div className='min-h-screen bg-[color:var(--blue)] text-white'>
-        <div className='container min-h-screen mx-auto px-8 flex flex-col'>
-          <div className='my-auto flex flex-col justify-center items-center'>
-            <div className='text-8xl font-thin mb-8'>
-              <span className='text-[color:var(--lighter-blue)]'>Sch</span>edu
-            </div>
-            <button
-              onClick={() => signIn()}
-              className='rounded-full px-8 py-2 text-sm font-light border hover:border-blue-200 hover:text-blue-200 duration-100'
-            >
-              sign-in with ITKMITL account
+  useEffect(() => {
+    const getToken = () => {
+      const token = getCookieValue(document.cookie, "SCHEDU_FBIDTOKEN")
+      if (token) updateSignState(true)
+    }
+    getToken()
+  }, [])
+
+  const renderActionPanel = () => {
+    // Already sign-in
+    if (isSigned) {
+      return (
+          <>
+          <div className='mb-6'>
+            กำลังอยู่ในระบบ ต้องการเข้าสู่หน้าหลักหรือไม่
+          </div>
+          <div className='flex items-center'>
+            <button className='rounded-full px-8 py-2 text-sm font-light border hover:border-blue-200 hover:text-blue-200 duration-100'>
+              เข้าสู่หน้าหลัก
+            </button>
+            <button className='rounded-full px-8 py-2 text-sm font-light border bg-rose-700 border-rose-700 hover:bg-rose-800 hover:border-rose-800 duration-100 ml-4'>
+              ออกจากระบบ
             </button>
           </div>
-          <div className='py-8 text-center text-sm font-light opacity-75'>
-            <div>made by skeptical saurus team</div>
+        </>
+      )
+    }
+
+    // Anonymous user
+    return (
+      <button
+        onClick={() => signIn()}
+        className='rounded-full px-8 py-2 text-sm font-light border hover:border-blue-200 hover:text-blue-200 duration-100'
+      >
+        เข้าสู่ระบบด้วยบัญชี ITKMITL
+      </button>
+    )
+
+  }
+
+  return (
+    <div className='min-h-screen bg-[color:var(--blue)] text-white'>
+      <div className='container min-h-screen mx-auto px-8 flex flex-col'>
+        <div className='my-auto flex flex-col justify-center items-center'>
+          <div className='text-8xl font-thin mb-8'>
+            <span className='text-[color:var(--lighter-blue)]'>Sch</span>edu
           </div>
+          {renderActionPanel()}
+        </div>
+        <div className='py-8 text-center text-sm font-light opacity-75'>
+          <div>made by skeptical saurus team</div>
         </div>
       </div>
-    )
+    </div>
+  )
 
 }
 
