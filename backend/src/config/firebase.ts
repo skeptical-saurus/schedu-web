aa6f84e5421e7c3b14e7653d39b3c4142f7959f9
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-04-19 19:48:42 +0700

    feat: add firebase app initializer

---
+++ backend/src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase-admin/app'
+
+const config = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
+}
+
+initializeApp(config)
