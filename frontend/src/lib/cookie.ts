07ba47222188da4510f7b15c5fd325a9acc82fa0
LoukHin <LoukHin@users.noreply.github.com>
2022-04-19 14:47:48 +0000

    Prettified Code!

--- frontend/src/lib/cookie.ts
+++ frontend/src/lib/cookie.ts
@@ -1,6 +1,6 @@
-export function getCookieValue(cookies:String, key:String) {
-    cookies = `; ${cookies}`
-    let parts = cookies.split(`; ${key}=`)
-    if (parts.length === 2) return parts.pop()?.split(';').shift()
-    return null
+export function getCookieValue(cookies: String, key: String) {
+  cookies = `; ${cookies}`
+  let parts = cookies.split(`; ${key}=`)
+  if (parts.length === 2) return parts.pop()?.split(';').shift()
+  return null
 }
