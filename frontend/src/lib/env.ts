bfa70f0fde66c6bf5014248cd3d286c098b97a65
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-04-19 20:42:17 +0700

    feat: verify email address before logging in

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -2,6 +2,7 @@ const env = {
   NODE_ENV: process.env.NODE_ENV,
   appName: process.env.NEXT_PUBLIC_APP_NAME,
   appUrl: process.env.NEXT_PUBLIC_APP_URL,
+  apiUrl: process.env.NEXT_PUBLIC_API_URL ?? '/api',
 }
 
 export default env
