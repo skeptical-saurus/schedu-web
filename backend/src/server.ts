afbbcbfe77a01d3d21f2ae4211b288429d8ffb48
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-04-19 20:14:41 +0700

    feat: add auth verification endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,16 @@
 import express from 'express'
 
+import 'config/firebase'
+import env from 'config/env'
+import authRouter from 'routes/auth'
+import { validateToken } from 'middlewares/auth'
+
 const app = express()
+app.disable('x-powered-by')
+app.use(express.json())
 
-const PORT = process.env.PORT
+app.use('/auth', validateToken, authRouter)
 
-app.listen(PORT, () => console.log(`Schedu server is listening on port ${PORT}`))
+app.listen(env.port, () => {
+  console.log(`Server is listening on port ${env.port}`)
+})
