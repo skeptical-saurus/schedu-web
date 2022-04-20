a5c9b652be0fd699d6a91e6cfb923b1869d7a82f
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-04-20 12:50:41 +0700

    create a search field

--- frontend/src/pages/contact/index.tsx
+++ frontend/src/pages/contact/index.tsx
@@ -3,6 +3,12 @@ import ContactCard from './components/contactCard'
 type Props = {}
 
 const Contact: React.FC<Props> = () => {
+
+  const searchHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let searchValue: String = event.target.value
+    // TODO: Filter the contact list
+  }
+
   return (
     <>
       <div className='font-bold mb-6 flex items-center'>
@@ -15,9 +21,15 @@ const Contact: React.FC<Props> = () => {
         <ContactCard />
         <ContactCard />
       </div>
-      <div className='font-bold mb-6 flex items-center'>
-        <span className='material-icons text-4xl mr-3'>connect_without_contact</span>
-        <span className='text-3xl'>รายชื่อผู้ติดต่อ</span>
+      <div className='flex justify-between items-center mb-6'>
+        <div className='font-bold flex items-center'>
+          <span className='material-icons text-4xl mr-3'>connect_without_contact</span>
+          <span className='text-3xl'>รายชื่อผู้ติดต่อ</span>
+        </div>
+        <div className='border rounded-full px-4 py-3 flex'>
+          <span className='material-icons mr-2'>search</span>
+          <input onChange={searchHandler} type='text' placeholder='ค้นหาผู้ติดต่อ' className='w-96 focus:outline-none' />
+        </div>
       </div>
     </>
   )
