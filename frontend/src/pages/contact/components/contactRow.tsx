e5c72805c26073c4495d029a7e7762a544decaff
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-04-20 23:27:41 +0700

    link to contact info page

--- frontend/src/pages/contact/components/contactRow.tsx
+++ frontend/src/pages/contact/components/contactRow.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 type Props = {}
 
 const ContactRow: React.FC<Props> = () => {
@@ -6,10 +8,12 @@ const ContactRow: React.FC<Props> = () => {
       <td>ไนน์แบคโฮ ฮอตอาข่า</td>
       <td>นักศึกษา</td>
       <td className='flex justify-end'>
-        <button className='border border-[color:var(--light-blue)] text-[color:var(--light-blue)] hover:border-[color:var(--blue)] hover:text-[color:var(--blue)] duration-100 pl-2 pr-3 rounded-full text-sm flex items-center'>
-          <span className='material-icons mr-1 text-lg'>watch_later</span>
-          <span>นัดหมาย</span>
-        </button>
+        <Link href='/contact/123'>
+          <button className='border border-[color:var(--light-blue)] text-[color:var(--light-blue)] hover:border-[color:var(--blue)] hover:text-[color:var(--blue)] duration-100 pl-2 pr-3 rounded-full text-sm flex items-center'>
+            <span className='material-icons mr-1 text-lg'>watch_later</span>
+            <span>นัดหมาย</span>
+          </button>
+        </Link>
       </td>
     </tr>
   )
