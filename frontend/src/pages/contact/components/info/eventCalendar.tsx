ce5a23e60ced30ff15d665027aefc43a47c7880c
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-05-15 16:43:23 +0700

    styling the calendar

--- frontend/src/pages/contact/components/info/eventCalendar.tsx
+++ frontend/src/pages/contact/components/info/eventCalendar.tsx
@@ -3,12 +3,12 @@ import Calendar from 'react-calendar'
 
 const EventCalendar: React.FC = () => {
 
-  const [value, onChange] = useState(new Date())
+  const [selected, changeDate] = useState<Date>()
 
   return (
     <>
       <div className='p-6 rounded-2xl border'>
-        <Calendar onChange={onChange} value={value} />
+        <Calendar onChange={changeDate} value={selected} />
       </div>
     </>
   )
