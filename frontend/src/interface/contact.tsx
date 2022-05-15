46a4b64d4570478553b0660f7be673aa3df5d0fd
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-05-15 12:36:58 +0700

    create a section for personal info

--- frontend/src/interface/contact.tsx
+++ frontend/src/interface/contact.tsx
@@ -2,5 +2,7 @@ export interface ContactInformation {
   id: string,
   firstname: string,
   lastname: string,
+  email: string,
+  tel: string,
   role: string
 }
--- frontend/src/pages/contact/components/info/personalInfo.tsx
+++ frontend/src/pages/contact/components/info/personalInfo.tsx
@@ -1,4 +1,5 @@
-import { ContactInformation } from "interface/contact"
+import { ContactInformation } from 'interface/contact'
+import { mapRoleTitle } from 'lib/contact'
 
 type Props = {
   contact: ContactInformation
@@ -8,11 +9,18 @@ const PersonalInfo: React.FC<Props> = ({ contact }) => {
 
   return (
     <>
-      <div className='flex items-center'>
-        <span className='material-icons text-6xl text-gray-600'>account_circle</span>
-        <div className='ml-2'>
-          <div>{contact.firstname} {contact.lastname}</div>
+      <div className='flex items-center mb-8'>
+        <span className='material-icons text-7xl text-gray-600'>account_circle</span>
+        <div className='ml-3 font-light'>
+          <div className='text-lg'>{contact.firstname} {contact.lastname}</div>
+          <div className='text-sm text-gray-600'>{mapRoleTitle(contact.role)}</div>
         </div>
+      </div>
+      <div className='grid grid-cols-5'>
+        <div className='col-span-2 py-3 border-b'>E-mail</div>
+        <div className='col-span-3 py-3 border-b font-light'>{contact.email}</div>
+        <div className='col-span-2 py-3 border-b'>เบอร์ติดต่อ</div>
+        <div className='col-span-3 py-3 border-b font-light'>{contact.tel}</div>
       </div>
     </>
   )
