1fb4c3528cb9b7096ed7ac038e41f3f3d55ef75e
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-05-15 12:20:17 +0700

    get data from separated mocked file

--- frontend/src/pages/contact/[uid].tsx
+++ frontend/src/pages/contact/[uid].tsx
@@ -1,10 +1,45 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import PersonalInfo from './components/info/personalInfo'
+import { ContactInformation } from 'interface/contact'
+
+import jsonUsers from 'mock/users.json'
+
 type Props = {}
 
 const ContactInfo: React.FC<Props> = () => {
 
+  const router = useRouter()
+  const { uid } = router.query
+
+  const [contact, setContact] = useState<ContactInformation>()
+
+  useEffect(() => {
+    const getContactInfo = async () => {
+      const result = jsonUsers.find(user => user.id === uid)
+      setContact(result)
+    }
+    getContactInfo()
+  }, [uid])
+
+  // Contact is not successfully loaded or not found any
+  if (!contact) return (
+    <>
+      <div className='text-center py-16 text-gray-500'>
+        <span className='material-icons text-9xl animate-spin'>autorenew</span>
+        <div className='font-light text-xl'>Loading ...</div>
+      </div>
+    </>
+  )
+
   return (
     <>
-      This is a contact info page
+      <div className='grid grid-cols-3'>
+        <div>
+          <PersonalInfo contact={contact!} />
+        </div>
+        <div className='col-span-2'></div>
+      </div>
     </>
   )
 }
