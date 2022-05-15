a1669c27beb3e180a4bfbe483be48a2601ce7441
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-05-15 20:53:34 +0700

    refactor!: remove create account endpoint

--- backend/src/graphql-schema/mutation/account.ts
+++ backend/src/graphql-schema/mutation/account.ts
@@ -1,13 +1,8 @@
 import { AccountTC } from 'models/account'
 
-export const createAccount = AccountTC.mongooseResolvers.createOne().wrapResolve((next) => (rp) => {
-  if (!rp.context.user.uid) throw new Error('Not allowed')
-  rp.args.record.googleId = rp.context.user.uid
-  return next(rp)
-})
-
 export const updateAccount = AccountTC.mongooseResolvers.updateOne().wrapResolve((next) => (rp) => {
-  if (rp.args.record.googleId && rp.args.record.googleId != rp.context.user.uid)
-    throw new Error('Not allowed')
+  rp.args.filter = {
+    googleId: rp.context.user.uid,
+  }
   return next(rp)
 })
