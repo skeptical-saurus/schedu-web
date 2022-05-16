20ac2cfca31d160935e4b7569810c1e286a4d8c7
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-05-16 19:09:57 +0700

    refactor!: use firebase config from env t pls

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -1,12 +1,20 @@
 import { initializeApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
-import firebaseConfig from '../../secret/firebaseConfig'
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+}
 
 if (!getApps().length) {
-  initializeApp(firebaseConfig)
+  initializeApp(config)
 }
 
 export const auth = getAuth()
 
-export default firebaseConfig
+export default config
