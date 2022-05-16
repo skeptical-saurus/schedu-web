641e7f4122a6ac079ec29a99e4bf365dff1caa72
l3luescreen <l3luescreen@users.noreply.github.com>
2022-05-16 10:09:36 +0000

    style: make code prettier (format code)

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,9 +3,8 @@ const env = {
   port: Number(process.env.PORT),
   mongoUri: process.env.MONGO_URI ?? '',
   options: {
-    dbName: process.env.SCHEDU_DB_NAME
-  }
+    dbName: process.env.SCHEDU_DB_NAME,
+  },
 }
 
 export default env
-
