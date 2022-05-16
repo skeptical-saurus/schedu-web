287e639f76d0179bc054faab7e99b583cd31e67b
l3luescreen <l3luescreen@users.noreply.github.com>
2022-05-16 08:02:30 +0000

    style: make code prettier (format code)

--- backend/src/config/mongoose-connect.ts
+++ backend/src/config/mongoose-connect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import env from 'config/env'
 
 const options = {
-    dbName: process.env.SCHEDU_DB_NAME
+  dbName: process.env.SCHEDU_DB_NAME,
 }
 
 export default mongoose.connect(`${env.mongoUri}`, options)
