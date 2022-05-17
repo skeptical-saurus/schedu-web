b2e432dd01a243865102a75ef3174f0df13bdeb6
l3luescreen <l3luescreen@users.noreply.github.com>
2022-05-17 05:55:13 +0000

    style: make code prettier (format code)

--- backend/src/graphql-schema/mutation/appointment.ts
+++ backend/src/graphql-schema/mutation/appointment.ts
@@ -3,4 +3,3 @@ import { AppointmentTC } from 'models/appointment'
 export const createAppointment = AppointmentTC.mongooseResolvers.createOne()
 
 export const updateAppointment = AppointmentTC.mongooseResolvers.updateOne()
-
