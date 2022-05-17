d5030a402775b1bbe20796b29ad52d026455be33
l3luescreen <hacky404@hotmail.com>
2022-05-17 12:44:22 +0700

    feat: can count appoints that relate to current user

--- backend/src/graphql-schema/query/appointment.ts
+++ backend/src/graphql-schema/query/appointment.ts
@@ -1,8 +1,23 @@
 import { AppointmentTC } from 'models/appointment'
 import { AccountModel } from 'models/account'
+import { count } from 'graphql-compose-mongoose/lib/resolvers/count'
 
 export const appointments = AppointmentTC.mongooseResolvers
   .findMany()
+  .wrapResolve((next) => async (rp) => {
+    const googleId = rp.context.user.uid
+
+    const target = await AccountModel.findOne({ googleId })
+
+    rp.args.filter = {
+      $or: [{ sender: target?._id }, { 'participants.userId': target?._id }],
+    }
+
+    return next(rp)
+  })
+
+export const countAppointment = AppointmentTC.mongooseResolvers
+  .count()
   .wrapResolve((next) => async (rp) => {
     const googleId = rp.context.user.uid
 
