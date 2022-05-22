89511e956b61baa97fb6ff6e61ff9146370daf9e
l3luescreen <hacky404@hotmail.com>
2022-05-22 13:01:10 +0700

    feat: delete appointment api again

--- backend/src/graphql-schema/mutation/appointment.ts
+++ backend/src/graphql-schema/mutation/appointment.ts
@@ -3,3 +3,5 @@ import { AppointmentTC } from 'models/appointment'
 export const createAppointment = AppointmentTC.mongooseResolvers.createOne()
 
 export const updateAppointment = AppointmentTC.mongooseResolvers.updateOne()
+
+export const deleteAppointment = AppointmentTC.mongooseResolvers.removeOne()
