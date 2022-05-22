569cf3d2fb011469de4bbd1d351d3c200126fb0d
l3luescreen <l3luescreen@users.noreply.github.com>
2022-05-22 16:22:52 +0000

    style: make code prettier (format code)

--- frontend/src/lib/apolloClient.ts
+++ frontend/src/lib/apolloClient.ts
@@ -2,9 +2,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 import env from './env'
 
 export const client = new ApolloClient({
-    uri: env.apiUrl + '/graphql',
-    cache: new InMemoryCache(),
-    headers: {
-        'id-token': 'Input token here'
-    }
+  uri: env.apiUrl + '/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    'id-token': 'Input token here',
+  },
 })
