784dc18e53ed9686ac3a7835bc079abb17422cfa
LoukHin <LoukHin@users.noreply.github.com>
2022-05-23 04:55:20 +0000

    style: make code prettier (format code)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}', './src/context/**/*.{ts,tsx}'],
+  content: [
+    './src/pages/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+    './src/context/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {},
     fontFamily: {
