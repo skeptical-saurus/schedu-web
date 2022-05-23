4dd779877e5dfb14376bf20721fc004dc6b026c9
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-05-23 11:44:09 +0700

    fix: missing content path for context

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
+  content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}', './src/context/**/*.{ts,tsx}'],
   theme: {
     extend: {},
     fontFamily: {
