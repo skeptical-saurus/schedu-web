7b07d16315796f7260200ad2843a0485ab707b9b
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-05-26 22:21:19 +0700

    feat: schedule new appointment on create

--- backend/src/graphql-schema/mutation/appointment.ts
+++ backend/src/graphql-schema/mutation/appointment.ts
@@ -1,24 +1,36 @@
+import { ResolverResolveParams, ResolverRpCb } from 'graphql-compose'
+import { scheduleAppointmentUpdate } from 'helpers/schedule'
 import { AccountModel } from 'models/account'
 import { AppointmentTC } from 'models/appointment'
+import { ApolloContext } from 'types/apollo'
+import { Appointment } from 'types/schemas'
 
 export const createAppointment = AppointmentTC.mongooseResolvers
   .createOne()
-  .wrapResolve((next) => async (rp) => {
-    if (!rp.context.user) {
-      throw Error('Unauthorized')
-    }
+  .wrapResolve(
+    (next: ResolverRpCb<Appointment, ApolloContext>) =>
+      async (rp: ResolverResolveParams<Appointment, ApolloContext>) => {
+        if (!rp.context.user) {
+          throw Error('Unauthorized')
+        }
 
-    const googleId = rp.context.user.uid
-    const self = await AccountModel.findOne({ googleId }).lean()
+        const googleId = rp.context.user.uid
+        const self = await AccountModel.findOne({ googleId }).lean()
 
-    rp.args.record = {
-      ...rp.args.record,
-      sender: self?._id,
-      status: 'pending',
-    }
+        rp.args.record = {
+          ...rp.args.record,
+          sender: self?._id,
+          status: 'pending',
+        }
 
-    return next(rp)
-  })
+        rp.beforeRecordMutate = async function (apm: Appointment) {
+          scheduleAppointmentUpdate(apm)
+          return apm
+        }
+
+        return next(rp)
+      }
+  )
 
 export const updateAppointment = AppointmentTC.mongooseResolvers.updateOne()
 
