5fee498e5d3dce325534c3b5644b2ed2cc264baf
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-05-26 23:38:20 +0700

    fix border color not change

--- frontend/src/lib/statusColor.ts
+++ frontend/src/lib/statusColor.ts
@@ -1,19 +1,14 @@
 export const colorizeStatus = (status: string) => {
   switch (status) {
     case 'pending':
-      console.log('1')
-      return 'border-amber-500'
+      return 'border-l-amber-500'
     case 'ongoing':
-      console.log('2')
-      return 'border-emerald-500'
+      return 'border-l-emerald-500'
     case 'done':
-      console.log('3')
-      return 'border-gray-700'
+      return 'border-l-gray-700'
     case 'abandoned':
-      console.log('4')
-      return 'border-rose-500'
+      return 'border-l-rose-500'
     default:
-      console.log('5')
-      return 'border-gray-700'
+      return 'border-l-gray-700'
   }
 }
