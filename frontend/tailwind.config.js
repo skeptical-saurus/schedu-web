89fe39fd3f371cdeef6e3df1a158dea22dc957db
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-05-28 09:53:25 +0700

    add lib dir in tailwind content

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/pages/**/*.{ts,tsx}',
     './src/components/**/*.{ts,tsx}',
     './src/context/**/*.{ts,tsx}',
+    './src/lib/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {},
