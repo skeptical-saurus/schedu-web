1f490ecfe443619e11ab20292747bb6b9ac0afda
Tasanai Srisawat <tasanai.srisawat@gmail.com>
2022-05-28 09:46:19 +0700

    fix status color not show correctly

--- frontend/src/lib/statusColor.ts
+++ frontend/src/lib/statusColor.ts
@@ -1,14 +1,14 @@
 export const colorizeStatus = (status: string) => {
   switch (status) {
     case 'pending':
-      return 'border-l-amber-500'
+      return 'border-l-4 border-l-amber-500'
     case 'ongoing':
-      return 'border-l-emerald-500'
+      return 'border-l-4 border-l-emerald-500'
     case 'done':
-      return 'border-l-gray-700'
+      return 'border-l-4 border-l-gray-700'
     case 'abandoned':
-      return 'border-l-rose-500'
+      return 'border-l-4 border-l-rose-500'
     default:
-      return 'border-l-gray-700'
+      return 'border-l-4 border-l-gray-700'
   }
 }
